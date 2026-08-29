bde47c969acc040f98cbf81de562067e148dc916
sebastien-violante <sebastien.violante@gmail.com>
2026-08-29 11:16:16 +0200

    bug fix absence event au chargement

--- dev-react-p11/src/pages/Home/index.js
+++ dev-react-p11/src/pages/Home/index.js
@@ -14,7 +14,8 @@ import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
   const {data} = useData()
-  const last = data.events[data.events.length - 1]
+  const last = data?.events?.at(-1)
+    
   return <>
     <header>
       <Menu />
